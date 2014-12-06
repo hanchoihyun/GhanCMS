24f1ce0b1acc6037e6d1e5ae8d5ca2a2123010f4
hanchoihyun <27704292@qq.com>
2014-12-06 13:34:03 +0800

    asdfsadf

--- themes/admini/views/layouts/main.php
+++ themes/admini/views/layouts/main.php
@@ -3,20 +3,6 @@ $this->beginContent('//layouts/header');
 $this->endContent();
 ?>
 
-<div id="content">
-    <!--breadcrumbs-->
-    <div id="content-header">
-        <div id="breadcrumb"> <a href="index.html" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a></div>
-    </div>
-    <!--End-breadcrumbs-->
-    <div id="content-header">
-        <div id="breadcrumb"> <a href="#" title="Go to Home" class="tip-bottom"><i class="icon-home"></i> Home</a> <a href="#" class="current">Tables</a> </div>
-        <h1>Tables</h1>
-    </div>
-    <?php
-    echo $content;
-    ?>
-</div>
 
 
 <?php
